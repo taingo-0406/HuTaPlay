2a5ba2525730ff1317aa71728fc114d9b33c2527
unknown <giahuy.dng@gmail.com>
2023-07-29 22:52:58 +0700

    add session start

--- access_game.php
+++ access_game.php
@@ -1,6 +1,7 @@
 <?php 
+    session_start();
+
     $current_stage = $_SESSION['current_stage'];
-    //check if current stage, if is odd redirect to game1.php, if is even redirect to game2.php
     if($current_stage % 2 == 0){
         header("Location: rubic.html");
     }else if ($current_stage % 2 == 1){
